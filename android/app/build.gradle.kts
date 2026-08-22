77ac3d94cb7c247a569e1ab0439eee341e7bacdf
positer <1225202347@qq.com>
2026-08-22 19:50:34 +0800

    release: prepare dev-0.5.5 version sync

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.newmark.mobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 504
-        versionName = "0.5.4"
+        versionCode = 505
+        versionName = "0.5.5"
     }
 
     buildTypes {
